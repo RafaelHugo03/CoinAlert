a831d1413f213cfd60842dee95d492a91a8a6538
Rafael Evangelista <rafaelhugo03@outlook.com>
2026-04-25 13:54:34 -0300

    add dependency injection and app configs

---
+++ backend/src/CoinAlertApi/IoC/DependencyInjector.cs
@@ -0,0 +1,66 @@
+using CoinAlertApi.Application.Workers;
+using CoinAlertApi.Application.Interfaces;
+using CoinAlertApi.Application.Services;
+using CoinAlertApi.Domain.Interfaces;
+using CoinAlertApi.Infrastructure.ExternalApis.CoinGecko;
+using CoinAlertApi.Infrastructure.Persistence;
+using CoinAlertApi.Infrastructure.Repositories;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace CoinAlertApi.IoC;
+
+public static class DependencyInjector
+{
+    public static void BindEnvironmentVariables(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<MongoDbSettings>(configuration.GetSection("MongoDB"));
+    }
+
+    public static void RegisterDatabase(this IServiceCollection services)
+    {
+        services.AddSingleton<IMongoClient>(sp =>
+        {
+            var settings = sp.GetRequiredService<IOptions<MongoDbSettings>>().Value;
+            return new MongoClient(settings.ConnectionString);
+        });
+
+        services.AddScoped<IMongoDatabase>(sp =>
+        {
+            var settings = sp.GetRequiredService<IOptions<MongoDbSettings>>().Value;
+            return sp.GetRequiredService<IMongoClient>().GetDatabase(settings.DatabaseName);
+        });
+    }
+
+    public static void RegisterRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IOpportunityRepository, OpportunityRepository>();
+    }
+
+    public static void RegisterServices(this IServiceCollection services)
+    {
+        services.AddScoped<IOpportunityService, OpportunityService>();
+    }
+
+    public static void RegisterHttpClients(this IServiceCollection services)
+    {
+        services.AddHttpClient("coingecko", c =>
+        {
+            c.BaseAddress = new Uri("https://api.coingecko.com");
+            c.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0");
+            c.DefaultRequestHeaders.Add("Accept", "application/json");
+        });
+
+        services.AddSingleton<CoinGeckoPriceClient>();
+    }
+
+    public static void RegisterSignalR(this IServiceCollection services)
+    {
+        services.AddSignalR();
+    }
+
+    public static void RegisterHostedServices(this IServiceCollection services)
+    {
+        services.AddHostedService<PriceMonitorService>();
+    }
+}
